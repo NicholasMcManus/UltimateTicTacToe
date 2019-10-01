6f6fb1a7adb23d04684e0ff7bb4d229d0330f61f
NicholasMcManus <mcmanusnj3@gmail.com>
2019-09-30 20:39:40 -0400

    Initial Commit

---
+++ src/View/View.java
@@ -0,0 +1,14 @@
+/*
+ * file name: View.java
+ * programmer name: Nick McManus
+ * date created: 09-30-2019
+ * date of last revision: 
+ * details of last revision:
+ * short description: 
+ */
+
+package View;
+
+public class View {
+
+}
