9e70d760a5dde36f890e45c84e05065e22f37f3a
NicholasMcManus <mcmanusnj3@gmail.com>
2019-10-03 12:38:09 -0400

    Added more initialization Files

---
+++ src/Application/Application.java
@@ -0,0 +1,20 @@
+/*
+ * file name: Application.java
+ * programmer name: Nick McManus
+ * date created: 10-03-2019
+ * date of last revision: 
+ * details of last revision:
+ * short description: 
+ */
+package Application;
+
+public class Application {
+
+    /**
+     * Main driver for the program
+     * @param args String array, currently unused
+     */
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Controller/Controller.java
@@ -0,0 +1,14 @@
+/*
+ * file name: Controller.java
+ * programmer name: Nick McManus
+ * date created: 10-03-2019
+ * date of last revision: 
+ * details of last revision:
+ * short description: 
+ */
+
+package Controller;
+
+public class Controller {
+
+}
