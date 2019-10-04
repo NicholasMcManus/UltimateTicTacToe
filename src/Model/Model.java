51815e5013547056e5efe7590f21fd8fbf573be8
NicholasMcManus <mcmanusnj3@gmail.com>
2019-10-04 13:40:24 -0400

    Cleaned up headings

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,12 +1,3 @@
-/*
- * file name: Model.java
- * programmer name: Nick McManus
- * date created: 10-03-2019
- * date of last revision: 
- * details of last revision:
- * short description: 
- */
-
 package Model;
 
 public class Model {
--- src/View/View.java
+++ src/View/View.java
@@ -1,12 +1,3 @@
-/*
- * file name: View.java
- * programmer name: Nick McManus
- * date created: 09-30-2019
- * date of last revision: 
- * details of last revision:
- * short description: 
- */
-
 package View;
 
 public class View {
