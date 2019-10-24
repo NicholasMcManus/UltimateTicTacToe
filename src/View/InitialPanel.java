4f89b72fd11d00459b58e9cea6e98a60f3b34a39
jxm6236 <54914673+jxm6236@users.noreply.github.com>
2019-10-23 20:27:16 -0400

    Added Initial Panel/Needs Updating

---
+++ src/View/InitialPanel.java
@@ -0,0 +1,59 @@
+
+/**
+	File name: InitialPanel.java
+	Short description:
+	IST 242 Assignment:
+	@author Jaemi Hammond
+	@version 1.01 Apr 16, 2019
+*/
+
+package View;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author jbh5619
+ */
+
+public class InitialPanel extends JPanel {
+    // attributes
+    private MainMenu mm;
+   
+    
+    public InitialPanel() {
+        super();
+        setLayout(null);
+        setSize(1200, 800);
+        
+        mm = new MainMenu();
+        add(mm);   
+        mm.setVisible(false);
+
+    }
+
+    // Set methods - one set method for each instance variable defined above
+    //             - purpose is to pass in a value stored in the private variable
+
+    // Get methods - one get method for each instance variable defined above
+    //             - purpose is to return the value stored in the private variable
+
+    public MainMenu getMm() {
+        return mm;
+    }
+    
+
+    // Additional methods -- such as for calculation, display
+    
+    public void setActionListeners(ActionListener al)
+    {
+        // add Action listeners to every button in every panel
+       mm.getHowtoButton().addActionListener(al);
+       mm.getQuitButton().addActionListener(al);
+       mm.getStartButton().addActionListener(al);
+    }
+
+}   //end class
